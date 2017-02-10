627579bb7a4e5f6caebd2a107dd47949eabf3a0e
Ryan Nguyen <hoangnn3007@gmail.com>
2017-02-10 14:15:55 +0700

    facedetect: add option to reduce size of image to reduce computation power
    Signed-off-by: Ryan Nguyen <hoangnn3007@gmail.com>

--- facedetection.cpp
+++ facedetection.cpp
@@ -5,23 +5,37 @@
 using namespace std;
 using namespace cv;
 
+#define REDUCE_SIZE	1
+
 FaceDetection::FaceDetection(const string &cascadePath)
 {
     if( !_cascade.load( cascadePath ) ){
         cerr << "--(!)Error loading face detection trainning file path\n" << cascadePath << endl;
-        _isValidCascade = false;
-    } else {
-        _isValidCascade = true;
+        exit(EXIT_FAILURE);
     }
 }
 
 void FaceDetection::detectFaces(Mat &frame, vector<Rect> &res) {
-    Mat frame_gray;
-    cvtColor( frame, frame_gray, CV_BGR2GRAY );
-    equalizeHist( frame_gray, frame_gray );
+	Mat frame_tmp;
+#if REDUCE_SIZE
+	/** Reduce size of image to reduce computation power */
+	resize(frame, frame_tmp, Size(), 0.5, 0.5, INTER_AREA);
+#else
+	frame_tmp = frame.clone();
+#endif
+    cvtColor( frame_tmp, frame_tmp, CV_BGR2GRAY );
+    equalizeHist( frame_tmp, frame_tmp );
 
     /*Detect faces*/
-    if (_isValidCascade) {
-        _cascade.detectMultiScale( frame_gray, res, 1.1, 2, 0|CV_HAAR_SCALE_IMAGE, Size(30, 30) );
-    }
+    _cascade.detectMultiScale( frame_tmp, res, 1.1, 2, 0|CV_HAAR_SCALE_IMAGE, Size(30, 30) );
+#if REDUCE_SIZE
+    for( size_t i = 0; i < res.size(); i++ ) {
+		if (((res[i].x + res[i].width) * 2 <= frame.size().width) && ((res[i].y + res[i].height) * 2 <= frame.size().height)) {
+			res[i].x = res[i].x * 2;
+			res[i].y = res[i].y * 2;
+			res[i].height = res[i].height * 2;
+			res[i].width = res[i].width * 2;
+		}
+	}
+#endif
 }
